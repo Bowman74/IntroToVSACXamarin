f93f911b7767b89e81d7611f2f7b9c2697ad80c3
Kevin Ford <4380198+Bowman74@users.noreply.github.com>
2018-01-09 22:24:51 -0500

    Fixed build script.

--- src/VSACXamarin/appcenter-post-build.sh
+++ src/VSACXamarin/appcenter-post-build.sh
@@ -1,4 +1,4 @@
-appcenter login -u $VSAC_usr -p $VSAC_pwd
+appcenter login --token $VSAC_token
 
 appcenter test run uitest --app "IntroToVSAC/XamarinApp-iOS" --devices 54c01546 --app-path "$APPCENTER_SOURCE_DIRECTORY/src/VSACXamarin/iOS/bin/iPhone/$APPCENTER_BRANCH/VSACXamarin.iOS.ipa"  --test-series "master" --locale "en_US" --build-dir "$APPCENTER_SOURCE_DIRECTORY/src/VSACXamarin/VSACXamarin.UITest.Ios/bin/Release"
 
