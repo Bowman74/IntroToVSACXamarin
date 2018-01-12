0b71c83a341f0d0bd43456f9b4e20065cd9cc70f
Kevin Ford <4380198+Bowman74@users.noreply.github.com>
2018-01-11 19:56:25 -0500

    Add logout.

--- src/VSACXamarin/appcenter-post-build.sh
+++ src/VSACXamarin/appcenter-post-build.sh
@@ -2,3 +2,5 @@ appcenter login --token $VSAC_token
 
 appcenter test run uitest --app "IntroToVSAC/XamarinApp-iOS" --devices 54c01546 --app-path "$APPCENTER_SOURCE_DIRECTORY/src/VSACXamarin/iOS/bin/iPhone/$APPCENTER_BRANCH/VSACXamarin.iOS.ipa"  --test-series "master" --locale "en_US" --build-dir "$APPCENTER_SOURCE_DIRECTORY/src/VSACXamarin/VSACXamarin.UITest.Ios/bin/Release"
 
+appcenter logout
+
